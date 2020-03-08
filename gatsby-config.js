ed879e1f7d491fe9b484abfff25dffbb28d7e47d
Ilya Sidorchik <ilya@sidorchik.ru>
2020-03-09 00:00:43 +0300

    Update config for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,37 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter X`,
-    description: `Kick off your project with this extended boilerplate. This starter ships with the main Gatsby configuration, Typescript and SCSS. Also you won’t be puzzled with clean consistent code because Husky library beautifies bad commits by using Prettier and Stylelint.`,
-    author: `@ilyasidorchik`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-sass`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+	pathPrefix: '/homework-css-dark-mode',
+	siteMetadata: {
+		title: `Gatsby Default Starter X`,
+		description: `Kick off your project with this extended boilerplate. This starter ships with the main Gatsby configuration, Typescript and SCSS. Also you won’t be puzzled with clean consistent code because Husky library beautifies bad commits by using Prettier and Stylelint.`,
+		author: `@ilyasidorchik`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		`gatsby-plugin-typescript`,
+		`gatsby-plugin-sass`,
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
 }
